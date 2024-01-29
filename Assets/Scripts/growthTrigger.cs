47078ee9e7247d0b4000a66722b7080254ec1f2d
zipzinger <zipzinger@devmotron.home>
2024-01-28 19:09:40 -0600

    feat: growthTrigger

--- Assets/Scripts/growthTrigger.cs
+++ Assets/Scripts/growthTrigger.cs
@@ -1,11 +1,27 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class growthTrigger : MonoBehaviour {
 
-    private void OnTriggerEnter(Collider other)
+    bool doingStuff = false;
+
+    private async Task OnTriggerEnter(Collider other)
     {
-        speed = speed * -1;
+        Debug.Log("other.gameobject.tag: " + other.gameObject.tag);
+        if(other.gameObject.tag == "Player" && !doingStuff) {
+            doingStuff = true;
+            Debug.Log("on trigger enter");
+            await AIDuckManager.Instance.ConsumeAllDucks();
+            //scale upu
+
+            //increase damage
+
+            GameManager.Instance.LevelUpPLayer();
+            //increase camera distance
+
+            Destroy(this.gameObject);
+        }
     }
 }
